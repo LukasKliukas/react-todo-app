f0511e2d3b1ddac0b42dc1247701d417c78235c1
Lukas Kliukas <lukaskliukas@gmail.com>
2022-06-27 10:58:52 +0300

    Main name edited

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -41,7 +41,7 @@ function TodoList() {
 
   return (
     <div>
-      <h1>What's the plan for today ?</h1>
+      <h1>What are your tasks ?</h1>
       <TodoForm onSubmit={addTodo} />
       <Todo
         todos={todos}
